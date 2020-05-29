bc14f7de0ca7c020a8e31f3c36ec93d45b831b26
ncavchl <ncavchl@ajou.ackr>
2020-05-29 13:58:34 +0900

    정리ㄹ

--- src/main/java/org/example/springboot/Application.java
+++ src/main/java/org/example/springboot/Application.java
@@ -6,6 +6,7 @@ SpringBootApplication;
 @SpringBootApplication
 //프로젝트의 메인 클래스
 //내장 WAS  - 언제 어디서나 같은 환경에 스프링 부트 배포 가능
+//
 public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
