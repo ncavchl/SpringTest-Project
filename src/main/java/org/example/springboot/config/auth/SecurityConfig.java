de650c9dc5361ca8f36c68d41cefcc54089893ec
ncavchl <ncavchl@ajou.ackr>
2020-06-12 07:45:03 +0900

    집가서하기org.springframework.security.oauth2.client.registration.ClientRegistrationRepository bean 생성 필요

--- src/main/java/org/example/springboot/config/auth/SecurityConfig.java
+++ src/main/java/org/example/springboot/config/auth/SecurityConfig.java
@@ -15,6 +15,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        //ㄹㄹ
         http
                 .csrf().disable()
                 .headers().frameOptions().disable()
